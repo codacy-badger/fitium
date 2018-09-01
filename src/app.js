ebf87f221843fb40b4795a44062bd23a312aee18
Dalibor Lipovac <dlipovac012@gmail.com>
2018-09-01 20:34:10 +0200

    Helmet used as middleware

--- src/app.js
+++ src/app.js
@@ -1,8 +1,14 @@
 import express from 'express';
+import helmet from 'helmet';
 import { loggerStream } from './utilities/logger';
 
 const app = express();
 
+/**
+ * Middlewares
+*/
 app.use(require('morgan')(':method :url :status :res[content-length] - :response-time ms', { stream: loggerStream }));
+
+app.use(helmet());
 
 export default app;
